8692b363bfc1fe880957828e2287ce884fdee30f
Andrius Uznys <div1@inbox.ru>
2016-07-10 20:54:02 +0300

    Lazy loading

--- SuperSimpleStocksApp/SuperSimpleStocksApp/ConcreteStockFactory.cs
+++ SuperSimpleStocksApp/SuperSimpleStocksApp/ConcreteStockFactory.cs
@@ -1,20 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SuperSimpleStocksApp
 {
     public class ConcreteStockFactory : StockFactory
     {
-        private Dictionary<Enums.StockType, Stock> stocks = new Dictionary<Enums.StockType, Stock>();
-
-        public ConcreteStockFactory()
-        {
-            stocks.Add(Enums.StockType.Common, new CommonStock());
-            stocks.Add(Enums.StockType.Preferred, new PreferredStock());
-        }
+        private readonly Lazy<Dictionary<Enums.StockType, Stock>> _stocks =
+            new Lazy<Dictionary<Enums.StockType, Stock>>(() =>
+            {
+                Dictionary<Enums.StockType, Stock> stocks = new Dictionary<Enums.StockType, Stock>
+                {
+                    {Enums.StockType.Common, new CommonStock()},
+                    {Enums.StockType.Preferred, new PreferredStock()}
+                };
+                return stocks;
+            });
 
         public override Stock GetStock(StockInitParams stockParams, IStockTrade stockTrade)
         {
-            var stock = stocks[stockParams.Type];
+            var stock = _stocks.Value[stockParams.Type];
             stock.SetStockProperties(stockParams.Symbol, stockParams.LastDividend, stockParams.FixedDividend,
                     stockParams.ParValue, stockTrade);
             return stock;
